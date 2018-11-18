c3cae5946c278ec5a9895e0fc4f2e8b666afac41
Samuel <ssitero@knights.ucf.edu>
2018-11-17 22:10:46 -0500

    .

---
+++ Final/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public float jumpforce;
+    private float moveInput;
+    private Rigidbody2D rb;
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate(){
+        moveInput = moveInput.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+}
