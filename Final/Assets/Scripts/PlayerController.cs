7ac5a8f5787da959d5073409fc293ed61eddccf3
Samuel <ssitero@knights.ucf.edu>
2018-11-17 22:38:10 -0500

    jumpdone

--- Final/Assets/Scripts/PlayerController.cs
+++ Final/Assets/Scripts/PlayerController.cs
@@ -5,17 +5,46 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     public float speed;
-    public float jumpforce;
+    public float jumpForce;
     private float moveInput;
+
     private Rigidbody2D rb;
 
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public int extraJumpsValue;
+    private int extraJumps;
+
     void Start(){
+        extraJumps = extraJumpsValue;
         rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate(){
-        moveInput = moveInput.GetAxis("Horizontal");
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+        moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+    }
+
+    void Update(){
+
+        if(isGrounded == true){
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true){
+            rb.velocity = Vector2.up * jumpForce;
+        }
     }
 
 }
